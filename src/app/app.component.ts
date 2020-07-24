e8125a763f648c095395cf802afcf6710eb0d492
prince2398 <princepiyush2398@gmail.com>
2020-07-24 12:01:48 +0530

    Code Styling and Comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,173 +1,181 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { NgxSpinnerService } from "ngx-spinner";
-import { HttpClient } from "@angular/common/http";
-import { DatePipe } from "@angular/common";
-import { faStar } from "@fortawesome/free-regular-svg-icons"
-import { faExclamationCircle, faChevronUp } from "@fortawesome/free-solid-svg-icons"
+import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import {
+  faExclamationCircle,
+  faChevronUp,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  
   title = 'gitStarred';
-  url = "https://api.github.com/search/repositories"
+  url = 'https://api.github.com/search/repositories'; //API url
   date = new Date();
-  query = "sort:>0 created:>";
+  query = 'sort:>0 created:>';
   nextPage;
   lastPage;
   repos = [];
   requestLimitReached = false;
-  limitReset;
-  noMoreRepos=false;
-  faStar = faStar;
-  faExclamationCircle = faExclamationCircle; 
-  faChevronUp = faChevronUp;
-
+  limitReset; //time to reset request limit reached timer
+  noMoreRepos = false;
+  faStar = faStar; //icon
+  faExclamationCircle = faExclamationCircle; //icon
+  faChevronUp = faChevronUp; //icon
 
   constructor(
-    private http:HttpClient, 
+    private http: HttpClient,
     private spinner: NgxSpinnerService,
     public datepipe: DatePipe
   ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.spinner.show();
 
-    this.loadInitialRepos().then(res=>{
-      this.spinner.hide();
-      this.loadNextPage();
-    },
-    rej=>{
-      if(this.requestLimitReached){
+    this.loadInitialRepos().then(
+      (res) => {
         this.spinner.hide();
+        this.loadNextPage();
+      },
+      (rej) => {
+        if (this.requestLimitReached) {
+          this.spinner.hide();
+        }
       }
-    });
+    );
   }
 
-  async loadInitialRepos(){
-    // Params
-    return new Promise( (resolve, reject)=>{
-      this.date.setDate(this.date.getDate()-30);
-      this.query = this.query + this.datepipe.transform(this.date, 'yyyy-MM-dd');
-      let params = { q: this.query,
-                      sort: "stars",
-                      per_page: "10",
-                      page: "1"
-      }
+  async loadInitialRepos() {
+    return new Promise((resolve, reject) => {
+      //Params
+      this.date.setDate(this.date.getDate() - 30);
+      this.query =
+        this.query + this.datepipe.transform(this.date, 'yyyy-MM-dd');
+      let params = { q: this.query, sort: 'stars', per_page: '10', page: '1' };
       // getting initial repos
-      this.http.get(this.url,{params:params, observe: "response"})
+      this.http
+        .get(this.url, { params: params, observe: 'response' })
         .subscribe(
-          resp =>{
-            if(resp.status == 200){
-              this.appendRepos(resp.body["items"]);
-              this.updateLinks(resp.headers.get("link"))
-              resolve()
+          (resp) => {
+            if (resp.status == 200) {
+              this.appendRepos(resp.body['items']);
+              this.updateLinks(resp.headers.get('link'));
+              resolve();
             }
           },
-          err=>{
+          (err) => {
             this.requestLimitReached = true;
-            this.limitReset = err.headers.get("X-Ratelimit-Reset") + "000";
+            this.limitReset = err.headers.get('X-Ratelimit-Reset') + '000';
             // console.log(err.headers.get("X-Ratelimit-Reset"));
             reject();
           }
-        )
+        );
     });
   }
-  
-  loadNextPage(){
-    
-    if(this.nextPage && !this.noMoreRepos){
-      this.http.get(this.nextPage,{ observe: "response"})
-        .subscribe(
-          resp =>{
-            if(resp.status == 200){
-              this.appendRepos(resp.body["items"]);
-              this.updateLinks(resp.headers.get("link"))
-              if(this.nextPage == this.lastPage){
-                this.noMoreRepos = true;
-                this.spinner.hide()
-              }
+
+  //Loading a next Page of Repositories and adding to the repos array
+  loadNextPage() {
+    if (this.nextPage && !this.noMoreRepos) {
+      this.http.get(this.nextPage, { observe: 'response' }).subscribe(
+        (resp) => {
+          if (resp.status == 200) {
+            this.appendRepos(resp.body['items']);
+            this.updateLinks(resp.headers.get('link'));
+            if (this.nextPage == this.lastPage) {
+              this.noMoreRepos = true;
+              this.spinner.hide();
             }
-          },
-          err=>{
-            this.requestLimitReached = true;
-            this.limitReset = err.headers.get("X-Ratelimit-Reset") + "000";
-            // console.log(err.headers.get("X-Ratelimit-Reset"));
-            this.spinner.hide()
-            // console.log(err);
           }
-        )
+        },
+        (err) => {
+          this.requestLimitReached = true;
+          this.limitReset = err.headers.get('X-Ratelimit-Reset') + '000';
+          this.spinner.hide();
+          // console.log(err);
+        }
+      );
     }
   }
 
-  updateLinks(links){
-    links.split(',').forEach( link =>{
-      if(link.split(";")[1] == ' rel="next"'){
-        this.nextPage = link.split(";")[0].replace("<","").replace(">","");
+  //Updating Next Page on Every request
+  updateLinks(links) {
+    links.split(',').forEach((link) => {
+      if (link.split(';')[1] == ' rel="next"') {
+        this.nextPage = link.split(';')[0].replace('<', '').replace('>', '');
       }
-      if(link.split(";")[1] == ' rel="last"'){
-        this.lastPage = link.split(";")[0];
+      if (link.split(';')[1] == ' rel="last"') {
+        this.lastPage = link.split(';')[0];
       }
-    })
-  } 
-
-  appendRepos(items){
-    items.forEach(item=>{
-      let repo ={};
-      repo["name"] = item["name"];
-      repo["html_url"] = item["html_url"];
-      repo["desc"] = item["description"];
-      repo["created"] = new Date(item["created_at"]);
-      repo["stars"] = item["stargazers_count"];
-      repo["issues"] = item["open_issues"]
-      repo["ownerName"] = item["owner"]["login"];
-      repo["ownerAvatar"] = item["owner"]["avatar_url"];
-      repo["ownerProfile"] = item["owner"]["html_url"];
-      repo["stars_url"] = repo["html_url"]+"/stargazers";
-      repo["issues_url"] = repo["html_url"]+"/issues";
-      this.repos.push(repo);
-    })
+    });
   }
-  @HostListener("window:scroll", []) onWindowScroll() {
+
+  appendRepos(items) {
+    items.forEach((item) => {
+      let repo = {};
+      repo['name'] = item['name'];
+      repo['html_url'] = item['html_url'];
+      repo['desc'] = item['description'];
+      repo['created'] = new Date(item['created_at']);
+      repo['stars'] = item['stargazers_count'];
+      repo['issues'] = item['open_issues'];
+      repo['ownerName'] = item['owner']['login'];
+      repo['ownerAvatar'] = item['owner']['avatar_url'];
+      repo['ownerProfile'] = item['owner']['html_url'];
+      repo['stars_url'] = repo['html_url'] + '/stargazers';
+      repo['issues_url'] = repo['html_url'] + '/issues';
+      this.repos.push(repo);
+    });
+  }
+
+  //For Scroll to Top Button
+  @HostListener('window:scroll', []) onWindowScroll() {
     this.scrollToTop();
   }
-  scrollToTop(){
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll-to-top").style.display = "block";
+  scrollToTop() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById('scroll-to-top').style.display = 'block';
     } else {
-        document.getElementById("scroll-to-top").style.display = "none";
+      document.getElementById('scroll-to-top').style.display = 'none';
     }
   }
-  toTop(){
+  toTop() {
     (function smoothscroll() {
-      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
       if (currentScroll > 0) {
-          window.requestAnimationFrame(smoothscroll);
-          window.scrollTo(0, currentScroll - (currentScroll / 8));
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
       }
     })();
   }
 
-  timerUp(event){
-    if (event.action == "done") { 
+  // Request Timer Reset
+  timerUp(event) {
+    if (event.action == 'done') {
       this.requestLimitReached = false;
-      if ( this.nextPage){
+      if (this.nextPage) {
         this.onScroll();
-      }else{
-        window.location.reload()
+      } else {
+        window.location.reload();
       }
-      // console.log("Finished"); 
+      // console.log("Finished");
     }
   }
-  onScroll(){
-    if(!this.requestLimitReached && !this.noMoreRepos){
+
+  //Loading new repos on scrolling down
+  onScroll() {
+    if (!this.requestLimitReached && !this.noMoreRepos) {
       this.loadNextPage();
       this.spinner.show();
-    }else{
+    } else {
       this.spinner.hide();
     }
   }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { NgxSpinnerModule } from "ngx-spinner";
-import { HttpClientModule } from "@angular/common/http";
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { DatePipe } from "@angular/common";
-import { CountdownModule } from "ngx-countdown";
+import { DatePipe } from '@angular/common';
+import { CountdownModule } from 'ngx-countdown';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     InfiniteScrollModule,
     NgxSpinnerModule,
     HttpClientModule,
     CountdownModule,
-    FontAwesomeModule
+    FontAwesomeModule,
   ],
-  providers: [ 
-    DatePipe 
-  ],
-  bootstrap: [AppComponent]
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
